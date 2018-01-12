d973b493f0b9a1ee23fbe0018d7ffced1a7c10ad
Denis Ryan <DenisRyan@protonmail.com>
2018-01-12 13:04:57 +0000

    Added variable "std. deviation inflammation" to data_structures.R

--- scripts/data_structures.R
+++ scripts/data_structures.R
@@ -5,3 +5,13 @@ cats <- read.csv(file= "data/feline_data.csv")
 ## Preliminary analysis of inflammation
 ##Load data (no headers, CSV)
 data <- read.csv(file= "data/inflammation-01.csv", header = FALSE)
+
+##Calculate mean info by patient and day
+avg_inflammation_patient <- rowMeans(data)
+avg_inflammation_day <- apply(data, 2, mean)
+
+##Calculate max daily inflammation
+max_day_inflammation <- apply(data, 2, max)
+
+##Calculate std. dev. by day
+std_deviation_inflammation <- apply(data, 2, sd)
