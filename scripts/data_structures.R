a8ae89b0dccce4a2f255e6a503564fdd684a347b
Denis Ryan <DenisRyan@protonmail.com>
2018-01-12 12:10:08 +0000

    Added inflammation.csv read to data_structures.R

--- scripts/data_structures.R
+++ scripts/data_structures.R
@@ -1,3 +1,7 @@
 ## Script for storing data structures
 ## Load in CSV data file as a data frame 
 cats <- read.csv(file= "data/feline_data.csv")
+
+## Preliminary analysis of inflammation
+##Load data (no headers, CSV)
+data <- read.csv(file= "data/inflammation-01.csv", header = FALSE)
